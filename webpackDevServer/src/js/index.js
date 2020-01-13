9b41745d5b165306004d247b891a93fc6e591b24
DLI <sistemas@dli.com.co>
2020-01-12 21:59:01 -0500

    feat: Hot Module Replacement

--- webpackDevServer/src/js/index.js
+++ webpackDevServer/src/js/index.js
@@ -1,3 +1,11 @@
 import '../css/index.css'
+import text from './text'
 
-document.body.innerHTML = '<p>Hola mundo desde Webpack! home</p>'
+text()
+
+if (module.hot) {
+  module.hot.accept('./text.js', function() {
+    console.log('he recargado en caliente')
+    text()
+  })
+}
